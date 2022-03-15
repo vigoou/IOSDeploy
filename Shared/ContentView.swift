a1bc13f098dd45c0f24520cf6480b3c6a6a1d786
Nguyen Le Tuyet Mai <tuyetmai@Nguyens-MacBook-Pro.local>
2022-03-15 15:47:59 +0700

    first commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Nguyen Le Tuyet Mai on 15/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/merchandiseios_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  merchandiseios_testApp.swift
+//  Shared
+//
+//  Created by Nguyen Le Tuyet Mai on 15/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct merchandiseios_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
